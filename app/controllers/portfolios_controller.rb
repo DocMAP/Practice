8742c1326c3af4aa9b20482426040f4863a49caf
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-11 12:53:03 -0700

    not actually saving to portfolios from new

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,22 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
 end
+
+  def create
+    @portfolio_item = Portfolio.new(params)
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolio_url(@portfolios), notice: "Portfolio Item was successfully created." }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
