67d6b443d4fcc29fde6212ab7bb9c1927e11b7b4
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-11 19:07:49 -0700

    still not actually saving

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,15 +9,12 @@ class PortfoliosController < ApplicationController
 end
 
   def create
-    @portfolio_item = Portfolio.new(params)
+    @portfolio_item = Portfolio.new
 
     respond_to do |format|
       if @portfolio_items.save
         format.html { redirect_to portfolio_url(@portfolios), notice: "Portfolio Item was successfully created." }
-        format.json { render :show, status: :created, location: @portfolio_item }
-      else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
