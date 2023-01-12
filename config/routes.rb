7321746c90245547da49cc302b372e96d3fb5c2a
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-12 15:21:59 -0700

    Friendly ID

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios
 
-  get 'about' to: 'pages#about'
-  get 'contact' to: 'pages#contact'
-
+  get 'pages/about'
+  get 'pages/contact'
+  
   resources :blogs
 
   root to: 'pages#home'
