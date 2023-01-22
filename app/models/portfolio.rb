56566308c82a74526192be1e9947cb26a3c4ec95
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-22 15:29:12 -0700

    implemented custom scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	def self.react
+		where(subtitle: "React")
+	end
+
+	#same as above written differently
+	scope :ruby_on_rails, ->{ where(subtitle: 'Ruby on Rails') }
 end
