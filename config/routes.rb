99bbb4374640a8a90a2f12d3c52a882a30dcf852
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-25 19:26:14 -0700

    styled homepage

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ end
   #globbing allows for anything after portfolios/ in the browser to redirect
 
   get 'about', to: 'pages#about'
-
+  get 'home', to: 'pages#home'
   get 'contact', to: 'pages#contact', as: 'info' 
   #only changes the method name to 'info' in routes not the view
   
