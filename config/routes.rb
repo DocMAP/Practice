e649eda3a5aebeebfee9f2a954998713eb3839ab
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-25 17:02:54 -0700

    custom lgin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
 #nested routes - remember to change the directories in controllers and views:
 #(create new admin folder and move dashboard controller inside)
