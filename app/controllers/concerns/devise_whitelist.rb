842e613d41c478b027773945a053700d00cee33a
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-27 15:29:03 -0700

    added name to devise

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,9 +1,7 @@
 module DeviseWhitelist
 	extend ActiveSupport::Concern
-
-	include do
-		before_action :configure_permitted_parameters, if: :devise_controller?
-	end
+	
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
