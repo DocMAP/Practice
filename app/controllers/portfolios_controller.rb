fd9ebc7d4b4ab1fd18c329d3174bc12b6e057106
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-01-31 11:00:01 -0700

    formatting changes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -52,6 +52,8 @@ class PortfoliosController < ApplicationController
     end
   end
 
+private
+
   def portfolio_params
     params.require(:portfolio). permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:name])
   end
