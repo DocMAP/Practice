92b6b901f0536cefc9cbdcb71141f6436fcd3757
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-02-02 16:27:59 -0700

    moved style to Rails

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,7 +29,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully created." }
+        format.html { redirect_to blogs_url(@blogs), notice: "Blog was successfully created." }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
