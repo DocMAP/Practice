46c193bacc16d9eb3afea9822f01069542f8e854
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-02-17 11:46:18 -0700

    not merging into master

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+import { FetchRequest } from '@rails/request.js'
+
+// Connects to data-controller="sortable"
+export default class extends Controller {
+  connect() {
+    console.log("Sortable controller connected")
+    this.sortable = Sortable.create(this.element, {
+      animation: 150,
+    })
+  }
+}
